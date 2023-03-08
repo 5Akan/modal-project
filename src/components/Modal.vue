8322ff85d064886fb3b34e572038c3c79af08a81
akanimoh <oakanimoh@gmail.com>
2023-03-08 19:14:20 +0100

    Using scoped to localise styles and a separate css file for global css

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="backdrop">
         <div class="modal">
+            <h1>Modal Title</h1>
             <p>Modal content</p>
         </div>
     </div>
 </template>
 
-<style>
+<style scoped>
     .modal{
         width:400;
         padding: 20px;
@@ -20,5 +21,10 @@
         background: rgba(0, 0, 0, 0.5);
         width: 100%;
         height: 100%;
+    }
+    h1{
+        color: #03cfb4;
+        border: none;
+        padding: 0;
     }
 </style>

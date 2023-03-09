e7ff153f64591361315627fd480948accfa2e918
akanimoh <oakanimoh@gmail.com>
2023-03-09 17:02:17 +0100

    How to use Props, examples of Props and attribute binding to be more dynamic

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="backdrop">
-        <div class="modal">
-            <h1>Modal Title</h1>
-            <p>Modal content</p>
+        <div class="modal" :class = "{sale:theme === 'sale'}">
+            <h1>{{header}}</h1>
+            <p>{{text}}</p>
         </div>
     </div>
 </template>
 
+<script>
+export default {
+        props:['header','text','theme']
+    }
+</script>
+    
 <style scoped>
     .modal{
         width:400;
@@ -26,5 +32,12 @@
         color: #03cfb4;
         border: none;
         padding: 0;
+    }
+    .modal.sale{
+        background: crimson;
+        color: white;
+    }
+    .modal.sale h1{
+        color: white;
     }
 </style>

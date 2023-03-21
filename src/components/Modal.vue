3418b7a52664686a1641ed1be0e6f1b4bea4037b
akanimoh <oakanimoh@gmail.com>
2023-03-21 18:03:20 +0100

    Quick style to the slots

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="backdrop" @click.self  ="closeModal">
         <div class="modal" :class = "{sale:theme === 'sale'}">
-            <slot name="links"></slot>
-            <div class="action">
-
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
             </div>
         </div>
     </div>
@@ -21,7 +21,7 @@ export default {
     }
 </script>
     
-<style scoped>
+<style >
     .modal{
         width:400;
         padding: 20px;
@@ -41,6 +41,26 @@ export default {
         border: none;
         padding: 0;
     }
+    .modal h1{
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+    .modal p{
+        font-style: normal;
+    }
+    .modal .actions{
+        text-align: center;
+        margin: 30px 0 10px 0;
+    }
+    .modal .actions a{
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        text-decoration: none;
+        margin: 10px;
+    }
     .modal.sale{
         background: crimson;
         color: white;
@@ -48,4 +68,10 @@ export default {
     .modal.sale h1{
         color: white;
     }
+    .modal.sale .actions{
+        color: white;
+     }
+    .modal.sale .actions a{
+        color: white;
+     }
 </style>
